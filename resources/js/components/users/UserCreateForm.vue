9d75394c052c1f59cbf52e2cca804d0f249c9674
melvin-rulit <melvin.melvin.rulit@gmail.com>
2024-10-18 09:25:18 +0000

    Рефакторинг

--- resources/js/components/users/UserCreateForm.vue
+++ resources/js/components/users/UserCreateForm.vue
@@ -1,73 +1,81 @@
-<template>
-    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-        <div class="px-6 py-6 lg:px-8">
-            <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Добавление пользователя</h3>
-            <Alert :errors="errors" />
-            <Success :message="message" />
-            <form @submit="store">
-                <div class="grid md:grid-cols-3 md:gap-6">
-                    <div class="relative z-0 w-full mb-6 group">
-                        <TextInput title="Фамилия пользователя" v-model:value="user.lastName" type="text"/>
-                    </div>
-                    <div class="relative z-0 w-full mb-6 group">
-                        <TextInput title="Имя пользователя" v-model:value="user.firstName" type="text"/>
-                    </div>
-                    <div class="relative z-0 w-full mb-6 group">
-                        <TextInput title="Отчество пользователя" v-model:value="user.middleName" type="text"/>
-                    </div>
-                </div>
-                <div class="relative z-0 w-full mb-6 group">
-                    <Checkbox title="Пользователь активен?" name="enabled" v-model:checked="user.enabled"
-                              :checked="user.enabled" type="checkbox"/>
-                </div>
-                <div class="grid md:grid-cols-5 md:gap-6">
-                    <div class="relative z-0 w-full mb-6 group">
-                        <Select name="roleId" v-model:value="user.roleId" title="Роль" :values="userRoles"/>
-                    </div>
-                    <div class="relative z-0 w-full mb-6 group">
-                        <Select name="branchId" v-model:value="user.branchId" title="Филиал" :values="branches"/>
-                    </div>
-                    <div class="relative z-0 w-full mb-6 group">
-                        <TextInput title="Email" v-model:value="user.email" type="email"/>
-                    </div>
-                    <div class="relative z-0 w-full mb-6 group">
-                        <TextInput title="Номер телефона" v-model:value="user.phoneNumber" type="text"/>
-                    </div>
-                    <div class="relative z-0 w-full mb-6 group">
-                        <TextInput title="Пароль" v-model:value="user.password" type="password"/>
-                    </div>
-                </div>
-                <div class="relative z-0 w-100 mb-6 group">
-                    <DateInput title="Дата рождения" v-model:value="user.birthday" type="date"/>
-                </div>
-                <div class="grid md:grid-cols-2 md:gap-6">
-                    <div class="relative z-0 w-full mb-6 group">
-                        <TextInput title="Серия паспорта" v-model:value="user.passportSeries" type="text"/>
-                    </div>
-                    <div class="relative z-0 w-full mb-6 group">
-                        <TextInput title="Номер паспорта" v-model:value="user.passportNumber" type="text"/>
-                    </div>
-                </div>
-                <div class="relative z-0 w-full mb-6 group">
-                    <TextInput title="Кем выдан паспорт" v-model:value="user.passportNotes" type="text"/>
-                </div>
-                <div class="relative z-0 w-full mb-6 group">
-                    <TextInput title="Адрес регистрации" v-model:value="user.registrationAddress" type="text"/>
-                </div>
-<!--                <div class="relative z-0 w-full mb-6 group">-->
-<!--                    <Textarea title="Комментарий" v-model:value="user.comment"/>-->
+<!--<template>-->
+<!--    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">-->
+<!--        <div class="px-6 py-6 lg:px-8">-->
+<!--            <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Добавление пользователя</h3>-->
+<!--            <Alert :errors="errors" />-->
+<!--            <Success :message="message" />-->
+<!--            <form @submit="store">-->
+<!--                <div class="grid md:grid-cols-3 md:gap-6">-->
+<!--                    <div class="relative z-0 w-full mb-6 group">-->
+<!--                        <TextInput title="Фамилия пользователя" v-model:value="user.lastName" type="text"/>-->
+<!--                    </div>-->
+<!--                    <div class="relative z-0 w-full mb-6 group">-->
+<!--                        <TextInput title="Имя пользователя" v-model:value="user.firstName" type="text"/>-->
+<!--                    </div>-->
+<!--                    <div class="relative z-0 w-full mb-6 group">-->
+<!--                        <TextInput title="Отчество пользователя" v-model:value="user.middleName" type="text"/>-->
+<!--                    </div>-->
 <!--                </div>-->
-                <div class="mt-6 flex items-center justify-end gap-x-6">
-                    <router-link to="/users" type="button"
-                                 class="text-sm font-semibold leading-6 text-gray-900">Отмена
-                    </router-link>
-                    <button type="submit"
-                            class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                        Сохранить
-                    </button>
-                </div>
-            </form>
-        </div>
+<!--                <div class="relative z-0 w-full mb-6 group">-->
+<!--                    <Checkbox title="Пользователь активен?" name="enabled" v-model:checked="user.enabled"-->
+<!--                              :checked="user.enabled" type="checkbox"/>-->
+<!--                </div>-->
+<!--                <div class="grid md:grid-cols-5 md:gap-6">-->
+<!--                    <div class="relative z-0 w-full mb-6 group">-->
+<!--                        <Select name="roleId" v-model:value="user.roleId" title="Роль" :values="userRoles"/>-->
+<!--                    </div>-->
+<!--                    <div class="relative z-0 w-full mb-6 group">-->
+<!--                        <Select name="branchId" v-model:value="user.branchId" title="Филиал" :values="branches"/>-->
+<!--                    </div>-->
+<!--                    <div class="relative z-0 w-full mb-6 group">-->
+<!--                        <TextInput title="Email" v-model:value="user.email" type="email"/>-->
+<!--                    </div>-->
+<!--                    <div class="relative z-0 w-full mb-6 group">-->
+<!--                        <TextInput title="Номер телефона" v-model:value="user.phoneNumber" type="text"/>-->
+<!--                    </div>-->
+<!--                    <div class="relative z-0 w-full mb-6 group">-->
+<!--                        <TextInput title="Пароль" v-model:value="user.password" type="password"/>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--                <div class="relative z-0 w-100 mb-6 group">-->
+<!--                    <DateInput title="Дата рождения" v-model:value="user.birthday" type="date"/>-->
+<!--                </div>-->
+<!--                <div class="grid md:grid-cols-2 md:gap-6">-->
+<!--                    <div class="relative z-0 w-full mb-6 group">-->
+<!--                        <TextInput title="Серия паспорта" v-model:value="user.passportSeries" type="text"/>-->
+<!--                    </div>-->
+<!--                    <div class="relative z-0 w-full mb-6 group">-->
+<!--                        <TextInput title="Номер паспорта" v-model:value="user.passportNumber" type="text"/>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--                <div class="relative z-0 w-full mb-6 group">-->
+<!--                    <TextInput title="Кем выдан паспорт" v-model:value="user.passportNotes" type="text"/>-->
+<!--                </div>-->
+<!--                <div class="relative z-0 w-full mb-6 group">-->
+<!--                    <TextInput title="Адрес регистрации" v-model:value="user.registrationAddress" type="text"/>-->
+<!--                </div>-->
+<!--&lt;!&ndash;                <div class="relative z-0 w-full mb-6 group">&ndash;&gt;-->
+<!--&lt;!&ndash;                    <Textarea title="Комментарий" v-model:value="user.comment"/>&ndash;&gt;-->
+<!--&lt;!&ndash;                </div>&ndash;&gt;-->
+<!--                <div class="mt-6 flex items-center justify-end gap-x-6">-->
+<!--                    <router-link to="/users" type="button"-->
+<!--                                 class="text-sm font-semibold leading-6 text-gray-900">Отмена-->
+<!--                    </router-link>-->
+<!--                    <button type="submit"-->
+<!--                            class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">-->
+<!--                        Сохранить-->
+<!--                    </button>-->
+<!--                </div>-->
+<!--            </form>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</template>-->
+
+<template>
+    <div>
+        <Header title="Создание пользователя" />
+        <hr>
+
     </div>
 </template>
 
@@ -81,10 +89,11 @@ import Checkbox from "../forms/Checkbox.vue";
 import {BranchService} from "../../services/BranchService";
 import DateInput from "../forms/DateInput.vue";
 import Textarea from "../forms/Textarea.vue";
+import Header from "../Header.vue";
 
 export default {
     name: "UserCreateForm",
-    components: {DateInput, Checkbox, Select, Alert, TextInput, Success, Textarea},
+    components: {Header, DateInput, Checkbox, Select, Alert, TextInput, Success, Textarea},
     data: function () {
         return {
             loading: false,
